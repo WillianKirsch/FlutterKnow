a9473f0bd42b083715305d01150ae436791270e6
Willian Kirsch <willian.kirsch@ravex.com.br>
2021-03-02 08:11:56 -0300

    Organizada nas pastas

--- Cursos/#30DaysOfFlutter/material-components-flutter-codelabs/mdc_100_series/ios/Flutter/flutter_export_environment.sh
+++ Cursos/#30DaysOfFlutter/material-components-flutter-codelabs/mdc_100_series/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\GitHub\Cursos\FlutterKnow\Cursos\material-components-flutter-codelabs\mdc_100_series"
+export "FLUTTER_APPLICATION_PATH=C:\GitHub\FlutterKnow\Cursos\#30DaysOfFlutter\material-components-flutter-codelabs\mdc_100_series"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
